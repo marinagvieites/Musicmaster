8bc5ead46a6833111e9682ba17927daf789b666a
Marina Vieites <marinagvieites@gmail.com>
2023-09-11 17:39:42 -0300

    entrega 2

--- entrega1.js
+++ entrega1.js
@@ -1,54 +1,55 @@
-let bienvenida = confirm("Bienvenid@ a Music Master, entrenamiento para músicos. Regístrate para comenzar. Si ya tienes usuario pulsa cancelar");
-if (bienvenida == true) {
-    let nombre = prompt("¿Cuál es tu nombre?");
-    let apellido = prompt("¿Cuál es tu apellido?");
-    prompt("Hola " + nombre + " " + apellido + ", ¿qué instrumento tocás? ");
-    let estudio = confirm("¿Estudiás música actualmente en una institución?");
-    let institucion;
-    if (estudio ==true) { 
-        institucion = prompt("¿en cuál?");
-    }
-    let anos = prompt("¿Hace cúantos años que estudiás música?");
 
-    const  listaPreguntas1= [
-        "¿Cuántos tiempos dura una redonda?", 
-        "¿Cuántos tiempos dura una negra?",
-        "¿Cuántos cuerdas tiene una guitarra?",
-        "¿Cuántos tiempos suman 2 corcheas?",
-        "¿Cuántos tiempos suman 4 negras?",
-    ];
+function corregir() {
+    let cantidadRespuestasCorrectas = 0;
+        let respuesta1 = document.getElementById("respuesta1")
+    if (respuesta1.value == "4") {
+        cantidadRespuestasCorrectas++;
+    }
+    let respuesta2 = document.getElementById("respuesta2")
+    if (respuesta2.value == "1") {
+        cantidadRespuestasCorrectas++;
+    }
+    let respuesta3 = document.getElementById("respuesta3")
+    if (respuesta3.value == "1") {
+        cantidadRespuestasCorrectas++;
+    }
+    let respuesta4 = document.getElementById("respuesta4")
+    if (respuesta4.value == "8") {
+        cantidadRespuestasCorrectas++;
+    }
+    let respuesta5 = document.getElementById("respuesta5")
+    if (respuesta5.value == "3") {
+        cantidadRespuestasCorrectas++;
+    }
     
-    const  listaPreguntas2= [
-        "¿Cuántos tiempos duran 4 semicorcheas?", 
-        "¿Cuantos tiempos tiene un compás con 6 corcheas?",
-        "¿Cuántas notas tiene la escala de Do mayor?",
-        "¿Cuantas corcheas entran en 4 tiempos?",
-        "¿Cuantos tiempos tiene un compás con 8 semicorcheas?",
-    ];
-    let respuestas;
-    if (anos <=1) {
-        mostrarNivel(1);
-        respuestas = mostrarPreguntas(listaPreguntas1);
-    }
-    else if (anos <=2) { 
-        mostrarNivel(2);
-        respuestas = mostrarPreguntas(listaPreguntas2);
-    }
-    console.log(respuestas);
+    let nota = document.getElementById("nota");
+    nota.innerText = cantidadRespuestasCorrectas;
+
+    let resultados = {final : cantidadRespuestasCorrectas};
+    let resultadosJson = JSON.stringify(resultados);
+     window.localStorage.setItem("correccion", resultadosJson);
    
 
 }
-function mostrarNivel(nivel) {
-    confirm("Excelente. Comenzarás en el Nivel " + nivel);
+
+let correccionValorJson = window.localStorage.getItem("correccion");
+if (correccionValorJson) {
+    let correccionValorParsed = JSON.parse(correccionValorJson);
+    let correccionValor = correccionValorParsed.final;
+    document.getElementById("nota").innerText = correccionValor;
+
 }
 
-function mostrarPreguntas(listado){
-    let respuestas = [];
-    for(let i=0; i<=4; i++){
-        let respuesta = prompt(listado[i]);
-        respuestas.push(respuesta);
-    }
-    return respuestas;
+function reiniciar() {
+    document.getElementById("respuesta1").value = "";
+    document.getElementById("respuesta2").value = "";
+    document.getElementById("respuesta3").value = "";
+    document.getElementById("respuesta4").value = "";
+    document.getElementById("respuesta5").value = "";
+    document.getElementById("nota")
+    nota.innerText = "0";
+
+
 }
 
 
